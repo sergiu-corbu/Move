6752557ac23add8391aef2f616a3d68c36d672fc
Sergiu Corbu <sergiucorbu@tottos-iPhone.tapptitude>
2021-04-16 17:50:55 +0300

    user menu

--- Move/Views/MenuView.swift
+++ Move/Views/MenuView.swift
@@ -11,29 +11,39 @@ struct MenuView: View {
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(alignment: .leading) {
-                NavigationBar(title: "Hi, Sergiu!", avatar: "avatar-img", backButton: "chevron-left-purple", action: {}).padding(.top, 20)
-                    .padding(.bottom, 85)
+                navBar
                 historyView
+                menuOptions
+                Spacer()
             }
-            Spacer()
+            // Image("scooter-img")
+            //    .aspectRatio(contentMode: .fill)
         }
         .padding([.leading, .trailing], 24)
         .background(Color.white)
     }
     
+    var navBar: some View {
+        NavigationBar(title: "Hi, Sergiu!", avatar: "avatar-img", backButton: "chevron-left-purple", action: {})
+            .padding(.top, 20)
+            .padding(.bottom, 40)
+    }
     var historyView: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 28)
-                .foregroundColor(.red)
-               
-                .background(Image("history-background-img").resizable())
+                .foregroundColor(.lightPurple)
                 .frame(width: .infinity, height: 100)
+                .background(Image("history-background-img").resizable())
             HStack {
                 VStack(alignment: .leading) {
-                    Text("Trips")
+                    Text("History")
+                        .foregroundColor(.white)
+                        .font(.custom(FontManager.Primary.bold, size: 18))
                     Text("Total rides: 20")
+                        .foregroundColor(.fadePurple)
+                        .font(.custom(FontManager.Primary.medium, size: 16))
                 }
-                
+                Spacer()
                 Button(action: {
                    
                 }, label: {
@@ -49,11 +59,59 @@ struct MenuView: View {
                     .background(Rectangle()
                                     .foregroundColor(.coralRed)
                                     .cornerRadius(16))})
-            }
-           
-            
-               
+            }.padding([.leading, .trailing], 30)
+        }.padding(.bottom, 30)
+    }
+    
+    var menuOptions: some View {
+        VStack(alignment: .leading) {
+            //MenuItems(icon: "wheel-img", title: "General Settings", components: [MenuSubItems(title: "Account", url: <#T##String?#>)])
+            MenuItems(icon: "flag-img", title: "Legal", components: [SubMenuItems(title: "Terms and Conditions", url: "https://tapptitude.com", index: 0), SubMenuItems(title: "Privacy Policy", url: "https://tapptitude.com", index: 1)])
+            MenuItems(icon: "star-img", title: "Rate Us", components: [])
         }
+    }
+}
+
+struct MenuItems: View {
+    
+    let icon: String
+    let title: String
+    let components: [SubMenuItems]
+    
+
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            HStack(alignment: .top) {
+                Image(icon)
+                    .padding(.trailing, 16)
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .foregroundColor(.darkPurple)
+                        .font(.custom(FontManager.Primary.bold, size: 18))
+                    if components.count > 0 {
+                        ForEach(0..<components.count) { index in
+                            components[index]
+                                .padding([.top, .bottom], 15)
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SubMenuItems: View, Hashable {
+    let title: String!
+    let url: String!
+    let index: Int
+    var body: some View {
+        Link(destination: URL(string: url)!, label: {
+            Text(title)
+                .foregroundColor(.darkPurple)
+                .font(.custom(FontManager.Primary.regular, size: 16))
+        })
     }
 }
 
