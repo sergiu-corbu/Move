29c9cd5f4d75c9e4e53cea75922d12119e6affab
Sergiu Corbu <sergiucorbu@Sergius-MacBook-Pro.local>
2021-04-18 20:29:45 +0300

    bug fix

---
+++ Move/Model/Scooter.swift
@@ -0,0 +1,13 @@
+//
+//  Scooter.swift
+//  Move
+//
+//  Created by Sergiu Corbu on 18.04.2021.
+//
+
+import Foundation
+
+struct Scooter: Identifiable{
+    let id: String
+    let image: String
+}
