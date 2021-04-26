f9c7cbaa8e9fcd7f549ee466d997d286c489d795
Sergiu Corbu <sergiucorbu@tottos-iPhone.tapptitude>
2021-04-26 18:21:06 +0300

    1

---
+++ Move/Helpers/ValidationSuccess.swift
@@ -0,0 +1,56 @@
+//
+//  ValidationSuccess.swift
+//  Move
+//
+//  Created by Sergiu Corbu on 13.04.2021.
+//
+
+import SwiftUI
+
+struct ValidationSuccess: View {
+    
+    let onFindScooters: () -> Void
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            checkMark
+            mainText
+            Spacer()
+            exploreButton
+        }
+        .multilineTextAlignment(.center)
+        .padding([.leading, .trailing], 24)
+        .background(
+            Image("rect-background-img")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        )
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+    var mainText: some View {
+        Text("We've succesfully validated your driving license!")
+            .foregroundColor(.white)
+            .font(.custom(FontManager.Primary.bold, size: 34.0))
+            .padding(.top, 70)
+            .frame(maxWidth: .infinity)
+    }
+    
+    var checkMark: some View {
+        Image("checkmark-img")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 172, height: 172)
+    }
+    
+    var exploreButton: some View {
+        CallToActionButton(enabled: true, text: "Find scooters", action: { onFindScooters() }).padding(.bottom, 20)
+    }
+}
+
+struct ValidationSuccess_Previews: PreviewProvider {
+    static var previews: some View {
+        ValidationSuccess(onFindScooters: {})
+    }
+}
---
+++ Move/MENU/HistoryView.swift
@@ -0,0 +1,114 @@
+//
+//  HistoryView.swift
+//  Move
+//
+//  Created by Sergiu Corbu on 16.04.2021.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+   
+    let onBack: () -> Void
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            NavigationBar(title: "History", avatar: nil, backButton: "chevron-left-purple", action: {
+                onBack()
+            })
+            TripDetail() // foreach trip in trips...
+        }
+        .padding([.leading, .trailing], 24)
+        .background(Color.white)
+    }
+}
+
+struct TripDetail: View {
+    
+    var body: some View {
+        ZStack {
+            GeometryReader { geometry in
+                RoundedRectangle(cornerRadius: 29)
+                    .stroke(Color.darkPurple, lineWidth: 1)
+                    .overlay(
+                        HStack {
+                            RoundedRectangle(cornerRadius: 29)
+                                .fill(Color.fadePurple)
+                                .opacity(0.15)
+                                .frame(width: geometry.size.width / 1.55)
+                            Spacer()
+                        }//.padding(.all, 0.5)
+                    )
+            }
+            HStack {
+                tripBoundaries
+                Spacer()
+                tripTime
+            }
+        }
+    }
+    var tripBoundaries: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Text("From")
+                    .foregroundColor(.fadePurple)
+                    .font(.custom(FontManager.Primary.medium, size: 12))
+                Text("9776 Gutkowski Shores Suite 420 ")
+                    .foregroundColor(.darkPurple)
+                    .font(.custom(FontManager.Primary.bold, size: 14))
+                    .multilineTextAlignment(.leading)
+                    .padding(.bottom, 1)
+                    .frame(maxWidth: 180, alignment: .leading)
+                    .lineLimit(2)
+            }
+            VStack(alignment: .leading) {
+                Text("To")
+                    .foregroundColor(.fadePurple)
+                    .font(.custom(FontManager.Primary.medium, size: 12))
+                Text("261 Howell Gardhnkjnjj jjjjjjjjjgh")
+                    .foregroundColor(.darkPurple)
+                    .font(.custom(FontManager.Primary.bold, size: 14))
+                    .frame(maxWidth: 180, alignment: .leading)
+                    .lineLimit(3)
+            }.padding(.bottom, 5)
+        }
+        .padding(.leading, 25)
+        .padding([.top, .bottom], 15)
+    }
+    
+    var tripTime: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Text("Travel time")
+                    .foregroundColor(.fadePurple)
+                    .font(.custom(FontManager.Primary.medium, size: 12))
+                Text("00:42 min")
+                    .foregroundColor(.darkPurple)
+                    .font(.custom(FontManager.Primary.bold, size: 14))
+                    .padding(.bottom, 10)
+                    .frame(maxWidth: 90, alignment: .leading)
+                    .lineLimit(1)
+                Spacer()
+            }
+            VStack(alignment: .leading) {
+                Text("Distance")
+                    .foregroundColor(.fadePurple)
+                    .font(.custom(FontManager.Primary.medium, size: 12))
+                Text("7.8 km")
+                    .foregroundColor(.darkPurple)
+                    .font(.custom(FontManager.Primary.bold, size: 14))
+                    .frame(maxWidth: 80, alignment: .leading)
+                    .lineLimit(1)
+                Spacer()
+            }
+        }
+        .padding(.trailing, 10)
+        .padding([.top, .bottom], 15)
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView(onBack: {})
+    }
+}
